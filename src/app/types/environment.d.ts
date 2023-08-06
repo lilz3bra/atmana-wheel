79b3672c846e6bd2c86523b7448a4b4ca376a49e
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-08-06 17:19:45 -0300

    Typed .env variables

---
+++ src/app/types/environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_TWITCH_API_KEY: string;
+        NEXT_PUBLIC_BACKEND_URL: string;
+        NEXT_PUBLIC_REDIRECT_URL: string;
+        NEXT_PUBLIC_UPLOADS_URL: string;
+        TWITCH_API_SECRET: string;
+    }
+}
