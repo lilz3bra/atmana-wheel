04d5fcc8cd2ab041f73dd1b129c1bc12daed8baf
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-08-07 23:09:31 -0300

    Centralized giveaway interfaces

---
+++ src/app/types/giveaway.d.ts
@@ -0,0 +1,13 @@
+interface giveaway {
+    dbId: string;
+    twId: string;
+    name: string;
+    cost: number;
+    prize: string;
+    winner: string | null;
+    paid: boolean;
+}
+interface Entry {
+    name: string;
+    weight: number;
+}
