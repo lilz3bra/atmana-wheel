87db99170d692184a3774bdf421cb6d9a42a428f
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-08-09 09:31:45 -0300

    + testing types

--- src/app/types/environment.d.ts
+++ src/app/types/environment.d.ts
@@ -5,5 +5,9 @@ namespace NodeJS {
         NEXT_PUBLIC_REDIRECT_URL: string;
         NEXT_PUBLIC_UPLOADS_URL: string;
         TWITCH_API_SECRET: string;
+        NEXT_PUBLIC_TEST_TWITCH_URL: string;
+        NEXT_PUBLIC_TEST_TWITCH_CLIENT: string;
+        NEXT_PUBLIC_TEST_TWITCH_SECRET: string;
+        NEXT_PUBLIC_TEST_TWITCH_BROADCASTER: string;
     }
 }
