0c454a77563372208176d43381b9262df5fa51b1
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-10-08 23:38:39 -0300

    Commented out old code

--- app/(pages)/wheel/[id]/page.tsx
+++ app/(pages)/wheel/[id]/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import Modal from "./modal";
-import { collection, doc, getDoc, onSnapshot, query, updateDoc, where } from "firebase/firestore";
-import { db } from "../../firebase";
-import { getCookie } from "cookies-next";
+
 import { Icon } from "@fortawesome/fontawesome-svg-core";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Loading from "@/app/loading";
-import { UserAuth } from "@/app/context/AuthContext";
 
 export default function WheelPage({ params }: any) {
     const [users, setUsers] = useState<UsersList>();
@@ -17,39 +13,38 @@ export default function WheelPage({ params }: any) {
     const [raffle, setRaffle] = useState<giveaway>();
     const [redeemData, setRedeemData] = useState();
     const [userId, setUserId] = useState("");
-    const { user } = UserAuth();
 
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            await new Promise((resolve) => setTimeout(resolve, 250));
-            setLoading(false);
-            if (user !== null && typeof user !== "undefined") setUserId(user.uid);
-        };
-        checkAuthentication();
-    }, [user]);
+    // useEffect(() => {
+    //     const checkAuthentication = async () => {
+    //         await new Promise((resolve) => setTimeout(resolve, 250));
+    //         setLoading(false);
+    //         if (user !== null && typeof user !== "undefined") setUserId(user.uid);
+    //     };
+    //     checkAuthentication();
+    // }, [user]);
 
     const onClose = () => {
         setVisible(false);
     };
-    useEffect(() => {
-        const fetchRaffleData = async () => {
-            const docRef = doc(db, "giveaways", params.id);
-            const docSnapshot = await getDoc(docRef);
-            if (docSnapshot.exists()) {
-                const data = docSnapshot.data();
-                setRaffle({
-                    dbId: docSnapshot.id,
-                    twId: data.id,
-                    name: data.name,
-                    cost: data.cost,
-                    prize: data.prize,
-                    winner: data.winner,
-                    paid: data.paid,
-                });
-            }
-        };
-        fetchRaffleData();
-    }, [params]);
+    // useEffect(() => {
+    //     const fetchRaffleData = async () => {
+    //         // const docRef = doc(db, "giveaways", params.id);
+    //         // const docSnapshot = await getDoc(docRef);
+    //         if (docSnapshot.exists()) {
+    //             const data = docSnapshot.data();
+    //             setRaffle({
+    //                 dbId: docSnapshot.id,
+    //                 twId: data.id,
+    //                 name: data.name,
+    //                 cost: data.cost,
+    //                 prize: data.prize,
+    //                 winner: data.winner,
+    //                 paid: data.paid,
+    //             });
+    //         }
+    //     };
+    //     fetchRaffleData();
+    // }, [params]);
 
     const getParticipants = async () => {
         const result: UsersList = await getPages();
@@ -80,14 +75,14 @@ export default function WheelPage({ params }: any) {
 
     const getPages = async (cursor?: string, accumulatedData: UsersList = {}): Promise<UsersList> => {
         const after = typeof cursor === "undefined" ? "" : `&after=${cursor}`;
-        const cookie = "Bearer " + (process.env.NEXT_PUBLIC_COOKIE ? process.env.NEXT_PUBLIC_COOKIE : getCookie("access_token"));
+        // const cookie = "Bearer " + (process.env.NEXT_PUBLIC_COOKIE ? process.env.NEXT_PUBLIC_COOKIE : getCookie("access_token"));
         const broadcaster = process.env.NEXT_PUBLIC_TWITCH_BROADCASTER ? process.env.NEXT_PUBLIC_TWITCH_BROADCASTER : localStorage.getItem("id");
         const res = await fetch(
             `${process.env.NEXT_PUBLIC_TWITCH_URL}/channel_points/custom_rewards/redemptions?broadcaster_id=${broadcaster}&reward_id=${
                 raffle!.twId
             }&first=50&status=FULFILLED${after}`,
             {
-                headers: { "Client-id": process.env.NEXT_PUBLIC_TWITCH_API_KEY, Authorization: cookie },
+                // headers: { "Client-id": process.env.NEXT_PUBLIC_TWITCH_API_KEY, Authorization: cookie },
             }
         );
         const d = await res.json();
@@ -125,13 +120,13 @@ export default function WheelPage({ params }: any) {
     }, [raffle]);
 
     const deleteReward = async () => {
-        const cookie = "Bearer " + (process.env.NEXT_PUBLIC_COOKIE ? process.env.NEXT_PUBLIC_COOKIE : getCookie("access_token"));
+        // const cookie = "Bearer " + (process.env.NEXT_PUBLIC_COOKIE ? process.env.NEXT_PUBLIC_COOKIE : getCookie("access_token"));
         const broadcaster = process.env.NEXT_PUBLIC_TWITCH_BROADCASTER ? process.env.NEXT_PUBLIC_TWITCH_BROADCASTER : localStorage.getItem("id");
         const res = await fetch(
             `${process.env.NEXT_PUBLIC_TWITCH_URL}/channel_points/custom_rewards?broadcaster_id=${broadcaster}&id=${raffle?.twId}`, // TODO: Change url to real one and use variables
             {
                 method: "DELETE",
-                headers: { "client-id": process.env.NEXT_PUBLIC_TWITCH_API_KEY, authorization: cookie }, // TODO: change to our clientid and var token
+                // headers: { "client-id": process.env.NEXT_PUBLIC_TWITCH_API_KEY, authorization: cookie }, // TODO: change to our clientid and var token
             }
         );
         const d = await res.json();
@@ -144,39 +139,39 @@ export default function WheelPage({ params }: any) {
         setVisible(true);
     };
 
-    const updateDb = (winner: Array<Object>) => {
-        const docRef = doc(db, "giveaways", raffle!.dbId);
-        updateDoc(docRef, { winner });
-    };
+    // const updateDb = (winner: Array<Object>) => {
+    //     const docRef = doc(db, "giveaways", raffle!.dbId);
+    //     updateDoc(docRef, { winner });
+    // };
 
-    if (!user && !loading) {
-        return <div>You need to log in to see this page</div>;
-    } else {
-        return (
-            <div id="main-content" className="flex flex-col  justify-center items-center m-4">
-                {loading || typeof users === "undefined" ? (
-                    <Loading />
-                ) : !users ? (
-                    <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
-                        Raffle doesn't exist
+    // if (!user && !loading) {
+    //     return <div>You need to log in to see this page</div>;
+    // } else {
+    return (
+        <div id="main-content" className="flex flex-col  justify-center items-center m-4">
+            {loading || typeof users === "undefined" ? (
+                // <Loading />
+                // ) : !users ? (
+                <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
+                    Raffle doesn't exist
+                </div>
+            ) : (
+                <>
+                    <p className="text-center">Do you want to close the redeems and get the registered participants?</p>
+                    <div className="flex flex-row m-2 justify-between items-center">
+                        <button onClick={getParticipants} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit">
+                            <FontAwesomeIcon icon={faArrowsRotate} />
+                        </button>
+                        <button onClick={closeAndDraw} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
+                            Close and draw
+                        </button>
                     </div>
-                ) : (
-                    <>
-                        <p className="text-center">Do you want to close the redeems and get the registered participants?</p>
-                        <div className="flex flex-row m-2 justify-between items-center">
-                            <button onClick={getParticipants} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit">
-                                <FontAwesomeIcon icon={faArrowsRotate} />
-                            </button>
-                            <button onClick={closeAndDraw} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
-                                Close and draw
-                            </button>
-                        </div>
-                        <h1 className="font-bold text-xl text-center m-4">{Object.keys(users).length} Participants</h1>
-                        <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">{typeof users !== "undefined" ? <UsersElement /> : loading && <Loading />}</div>
-                        {visible && typeof users !== "undefined" ? <Modal entries={users} onClose={onClose} returnCallback={updateDb} /> : null}
-                    </>
-                )}
-            </div>
-        );
-    }
+                    {/* <h1 className="font-bold text-xl text-center m-4">{Object.keys(users).length} Participants</h1> */}
+                    {/* <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">{typeof users !== "undefined" ? <UsersElement /> : loading && <Loading />}</div> */}
+                    {/* {visible && typeof users !== "undefined" ? <Modal entries={users} onClose={onClose} returnCallback={updateDb} /> : null} */}
+                </>
+            )}
+        </div>
+    );
+    // }
 }
