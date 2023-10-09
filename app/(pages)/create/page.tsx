88368119dd48688f679b350ccc4acafc769e7f22
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-10-08 23:51:19 -0300

    Another vercel fix

--- app/(pages)/create/page.tsx
+++ app/(pages)/create/page.tsx
@@ -3,8 +3,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
-const Create = () => {
-    const session = getServerSession(authOptions);
+const Create = async () => {
+    const session = await getServerSession(authOptions);
     if (!session) {
         redirect("api/auth/signin");
     }
