913a08200cebe5685580593b00bb725d111c161c
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-10-10 10:22:47 -0300

    Handle session, correcly display the login/out btn

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,72 +3,33 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { SignInButton } from "./Buttons";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
-    // const { user, googleSignIn, logOut } = UserAuth();
     const [loading, setLoading] = useState(true);
     const router = useRouter();
+    const { data: session, status } = useSession();
 
-    // const handleSingIn = async () => {
-    //     try {
-    //         await googleSignIn();
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-    // const handleSingOut = async () => {
-    //     try {
-    //         await logOut();
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     const checkAuthentication = async () => {
-    //         await new Promise((resolve) => setTimeout(resolve, 250));
-    //         setLoading(false);
-    //     };
-    //     checkAuthentication();
-    // }, [user]);
     return (
         <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
             <ul className="flex ">
                 <li className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" onClick={() => router.push(`/`)}>
                     Home
                 </li>
-                {/* {!user ? null : ( */}
-                <>
-                    <li className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" onClick={() => router.push(`/history`)}>
-                        History
-                    </li>
-                    <li className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" onClick={() => router.push(`/create`)}>
-                        Create
-                    </li>
-                    <li>
-                        {/* <TwitchConnect user={user} /> */}
-                        <SignInButton />
-                    </li>
-                </>
-                {/* )} */}
+                {status === "authenticated" && (
+                    <>
+                        <li className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" onClick={() => router.push(`/history`)}>
+                            History
+                        </li>
+                        <li className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" onClick={() => router.push(`/create`)}>
+                            Create
+                        </li>
+                    </>
+                )}
             </ul>
-            {/* {loading ? null : !user ? ( */}
-            {/* <ul className="flex">
-                    <li className="p2 cursor-pointer" onClick={handleSingIn}>
-                        Login
-                    </li>
-                </ul>
-            ) : (
-                <ul className="flex">
-                    <div className="p-2 flex align-middle ">
-                        <p className="p-2">Welcome, {user.displayName} </p>
-                        <Image src={user.photoURL} alt="User profile picture" width={32} height={32} className="rounded-full mx-2" />
-                        <p className="cursor-pointer p-2  hover:bg-blue-700 rounded-xl" onClick={handleSingOut}>
-                            Logout
-                        </p>
-                    </div>
-                </ul>
-            )} */}
+            <div className="flex flex-row gap-4">
+                <SignInButton />
+            </div>
         </div>
     );
 };
