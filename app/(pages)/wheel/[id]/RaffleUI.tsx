a801c2c404802140324b90e6f2efd7551ea42b8e
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-10-14 15:56:37 -0300

    +pause. Ch UI. + Delete. + Resuming. Fix  0 users

--- app/(pages)/wheel/[id]/RaffleUI.tsx
+++ app/(pages)/wheel/[id]/RaffleUI.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate, faPause, faPlay, faTicket, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 import Modal from "./modal";
@@ -17,14 +17,16 @@ const RaffleUI = ({ giveaway }: Props) => {
     const [loading, setLoading] = useState(true);
     const [visible, setVisible] = useState(false);
     const [raffle, setRaffle] = useState<giveaway>();
+    const [error, setError] = useState(false);
+    const [isPaused, setPaused] = useState(false);
     const [total, setTotal] = useState(0);
     const firstRun = useRef(true);
 
     const getParticipants = async () => {
         setLoading(true);
         const res = await fetch(`/api/raffle?raffleId=${giveaway.twitchId}`);
+        if (res.status !== 200) setError(true);
         const result = await res.json();
-        console.log(result);
         setUsers(result);
         sumTotals(result);
         setLoading(false);
@@ -44,15 +46,26 @@ const RaffleUI = ({ giveaway }: Props) => {
         const result = await res.json();
     };
 
-    const closeAndDraw = () => {
+    const pauseReward = async () => {
+        const res = await fetch(`/api/raffle?raffleId=${giveaway.twitchId}`, { method: "PATCH", body: JSON.stringify({ is_paused: true }) });
+        const result = await res.json();
+    };
+
+    const pauseAndDraw = () => {
         getParticipants();
-        deleteReward();
+        setPaused(true);
+        pauseReward();
         setVisible(true);
     };
 
     const updateDb = async (winner: Array<Object>) => {
         const res = await fetch(`/api/raffle?raffleId=${giveaway.id}`, { method: "POST", body: JSON.stringify({ winner: winner }) });
         const result = await res.json();
+    };
+
+    const reopen = async () => {
+        setPaused(false);
+        const res = await fetch(`/api/raffle?raffleId=${giveaway.twitchId}`, { method: "PATCH", body: JSON.stringify({ is_paused: false }) });
     };
 
     const onClose = () => {
@@ -63,39 +76,52 @@ const RaffleUI = ({ giveaway }: Props) => {
         if (firstRun.current === true) {
             firstRun.current = false;
             getParticipants();
+            console.log(users);
         }
     }, []);
 
+    if (loading) return <Loading />;
+    if (error) {
+        return (
+            <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
+                Raffle doesn't exist
+            </div>
+        );
+    }
+    if (!users || total === 0)
+        return (
+            <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
+                No entries yet
+            </div>
+        );
+
     return (
-        <div id="main-content" className="flex flex-col  justify-center items-center m-4">
-            {loading ? (
-                <Loading />
-            ) : !users || total === 0 ? (
-                <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
-                    Raffle doesn't exist
-                </div>
-            ) : (
-                <>
-                    <p className="text-center">Do you want to close the redemptions and get the registered participants?</p>
-                    <div className="flex flex-row m-2 justify-between items-center">
-                        <button onClick={getParticipants} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit">
-                            <FontAwesomeIcon icon={faArrowsRotate} />
-                        </button>
-                        <button onClick={closeAndDraw} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
-                            Close and draw
-                        </button>
-                    </div>
-                    <h1 className="font-bold text-xl text-center m-4">
-                        {Object.keys(users).length} Participants ({total} entries)
-                    </h1>
-                    <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">
-                        {typeof users !== "undefined" ? <ParticipantsList users={users} tot={total} /> : loading && <Loading />}
-                    </div>
-                    {visible && typeof users !== "undefined" ? <Modal entries={users} onClose={onClose} returnCallback={updateDb} /> : null}
-                </>
-            )}
-        </div>
+        <>
+            <p className="text-center">Do you want to close the redemptions and get the registered participants?</p>
+            <div className="flex flex-row m-2 justify-between items-center">
+                <button onClick={getParticipants} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit">
+                    <FontAwesomeIcon icon={faArrowsRotate} />
+                </button>
+                <button onClick={pauseAndDraw} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
+                    {!isPaused && <FontAwesomeIcon icon={faPause} />} <FontAwesomeIcon icon={faTicket} />
+                </button>
+                {isPaused && (
+                    <button onClick={reopen} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
+                        <FontAwesomeIcon icon={faPlay} />
+                    </button>
+                )}
+                <button onClick={deleteReward} className="rounded-xl bg-blue-500 hover:bg-blue-700 p-2 mx-2 w-fit ">
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+            </div>
+            <h1 className="font-bold text-xl text-center m-4">
+                {Object.keys(users).length} Participants ({total} entries)
+            </h1>
+            <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">
+                {typeof users !== "undefined" ? <ParticipantsList users={users} tot={total} /> : loading && <Loading />}
+            </div>
+            {visible && typeof users !== "undefined" ? <Modal entries={users} onClose={onClose} returnCallback={updateDb} /> : null}
+        </>
     );
 };
-
 export default RaffleUI;
