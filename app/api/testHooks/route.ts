75ba5ba25b41704c684a26fba362883248c0f0ee
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-02 21:37:11 -0300

    Fixed request

--- app/api/testHooks/route.ts
+++ app/api/testHooks/route.ts
@@ -22,14 +22,14 @@ export async function PUT(req: Request) {
     const thisUser = session.user;
 
     const eventSubCreateUrl = "https://api.twitch.tv/helix/eventsub/subscriptions";
-    const headers = { Authorization: `Bearer ${thisUser.access_token}`, "Client-Id": process.env.NEXT_PUBLIC_TWITCH_API_KEY };
+    const headers = { Authorization: `Bearer ${thisUser.access_token}`, "Client-Id": process.env.NEXT_PUBLIC_TWITCH_API_KEY, "Content-Type": "application/json" };
     const body = JSON.stringify({
         type: "channel.channel_points_custom_reward_redemption.add",
         version: "1",
         condition: { broadcaster_user_id: thisUser.providerAccountId },
         transport: { method: "webhook", callback: process.env.NEXT_PUBLIC_REDIRECT_URL, secret: process.env.TWITCH_API_SECRET },
     });
-    const result = await fetch(eventSubCreateUrl, { headers, body });
+    const result = await fetch(eventSubCreateUrl, { method: "POST", headers, body });
     const data = await result.json();
     return NextResponse.json(data);
 }
