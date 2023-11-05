37a84a1d9b43afcc81724703dd7916041e9047e7
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-05 20:48:18 -0300

    Create an eventsub and show the result

--- app/(pages)/testHooks/page.tsx
+++ app/(pages)/testHooks/page.tsx
@@ -1,13 +1,21 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const TestHooks = () => {
-    const createSub = () => {
+    const [result, setResult] = useState();
+    const [enabled, setEnabled] = useState(false);
+
+    const createSub = async () => {
         console.log("I will create an event sub. It will listen on the server");
+        const res = await fetch("/api/testHooks", { method: "PUT" });
+        const data = await res.json();
+        setResult(data);
     };
+
     return (
         <div>
-            <div></div>
+            <div>{JSON.stringify(result)}</div>
+            <input type="text" name="redemption" id="redemption" />
             <button onClick={createSub}>Click me</button>
         </div>
     );
