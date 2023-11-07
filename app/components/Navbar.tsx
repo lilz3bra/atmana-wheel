d1d74e010268fc707300e5ece2721ad3afd33f9d
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-06 23:16:53 -0300

    Updated with submenus + new paths (tbi)

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,22 +8,38 @@ const Navbar = async () => {
 
     return (
         <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
-            <ul className="flex ">
+            <ul className="flex flex-row gap-4">
                 <Link className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/">
                     Home
                 </Link>
                 {!!session && (
-                    <>
-                        <Link className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/history">
-                            History
-                        </Link>
-                        <Link className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/create">
-                            Create
-                        </Link>
-                        <Link className="p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/active">
+                    <div className="relative flex flex-row">
+                        <div className="group/wheel relative">
+                            <p className="p-2 cursor-pointer rounded-xl">Wheel</p>
+                            <div className="hidden group-hover/wheel:flex absolute top-full left-0 bg-black border rounded-md p-2">
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/wheel/history">
+                                    History
+                                </Link>
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/wheel/create">
+                                    Create
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="group/prompt relative">
+                            <p className="p-2 cursor-pointer rounded-xl">Prompts</p>
+                            <div className="hidden group-hover/prompt:flex absolute top-full left-0 bg-black border rounded-md p-2">
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/prompts/history">
+                                    History
+                                </Link>
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/prompts/create">
+                                    Create
+                                </Link>
+                            </div>
+                        </div>
+                        <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/active">
                             Active
                         </Link>
-                    </>
+                    </div>
                 )}
             </ul>
             <div className="flex flex-row gap-4">
