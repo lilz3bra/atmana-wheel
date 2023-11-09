6df95219ca04ad33c3b6933c955458964f797fde
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-08 23:18:47 -0300

    Updated the structure of links again

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,25 +14,17 @@ const Navbar = async () => {
                 </Link>
                 {!!session && (
                     <div className="relative flex flex-row">
-                        <div className="group/wheel relative">
-                            <p className="p-2 cursor-pointer rounded-xl">Wheel</p>
-                            <div className="hidden group-hover/wheel:flex absolute top-full left-0 bg-black border rounded-md p-2">
-                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/wheel/history">
-                                    History
-                                </Link>
-                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/wheel/create">
-                                    Create
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="group/prompt relative">
-                            <p className="p-2 cursor-pointer rounded-xl">Prompts</p>
+                        <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/create">
+                            Create
+                        </Link>
+                        <div className="group/prompt relative hover:bg-blue-700 px-2 rounded-xl">
+                            <p className="p-2  cursor-pointer rounded-xl">List</p>
                             <div className="hidden group-hover/prompt:flex absolute top-full left-0 bg-black border rounded-md p-2">
-                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/prompts/history">
-                                    History
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/history/prompt">
+                                    Prompts
                                 </Link>
-                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/prompts/create">
-                                    Create
+                                <Link className="block p-2 cursor-pointer hover:bg-blue-700 rounded-xl" href="/history/wheel">
+                                    Wheel
                                 </Link>
                             </div>
                         </div>
