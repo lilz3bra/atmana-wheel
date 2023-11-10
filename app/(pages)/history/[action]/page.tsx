ea3bc7b001665252903201683ca76829c3f16989
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-10 16:17:51 -0300

    + different queries for the modes

--- app/(pages)/history/[action]/page.tsx
+++ app/(pages)/history/[action]/page.tsx
@@ -5,21 +5,36 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma";
 
-const History = async () => {
+interface Props {
+    params: {
+        action: string;
+    };
+}
+const History = async ({ params }: Props) => {
     const session = await getServerSession(authOptions);
     if (!session) {
         redirect("api/auth/signin");
     }
     const currentUser = session?.user?.id;
+    let fetchedItems;
+    if (params.action === "prompt") {
+        fetchedItems = await prisma.prompts.findMany({
+            where: {
+                creatorId: currentUser,
+                hidden: false,
+            },
+        });
+    }
+    if (params.action === "wheel") {
+        fetchedItems = await prisma.giveaways.findMany({
+            where: {
+                creatorId: currentUser,
+                hidden: false,
+            },
+        });
+    }
 
-    const fetchedItems = await prisma.giveaways.findMany({
-        where: {
-            creatorId: currentUser,
-            hidden: false,
-        },
-    });
-
-    return <HistoryList items={fetchedItems} />;
+    return <HistoryList items={fetchedItems!} />;
 };
 
 export default History;
