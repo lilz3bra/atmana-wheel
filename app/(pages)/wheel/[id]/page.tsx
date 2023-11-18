170b5502d812e9486aedfd0cdced6c09040fe093
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-18 20:34:48 -0300

    Properly check if the current user is the creator

--- app/(pages)/wheel/[id]/page.tsx
+++ app/(pages)/wheel/[id]/page.tsx
@@ -15,22 +15,31 @@ export default async function WheelPage({ params }: Props) {
     if (!session) {
         redirect("api/auth/signin");
     }
-    const currentUser = session?.user?.id;
-
     const giveaway = await prisma.giveaways.findFirst({
         where: {
             id: params.id,
         },
-        select: { id: true, twitchId: true, paused: true },
+        select: { id: true, twitchId: true, paused: true, creatorId: true },
     });
-
+    // Fetch this user's id in the db
+    const thisUser = await prisma.account.findFirst({ where: { providerAccountId: session.user.providerAccountId }, select: { userId: true } });
     if (giveaway) {
-        return (
-            <div id="main-content" className="flex flex-col  justify-center items-center m-4">
-                <RaffleUI giveaway={giveaway!} />
-            </div>
-        );
+        // Check if the current user is the creator of the giveaway
+        if (thisUser?.userId === giveaway.creatorId) {
+            return (
+                <div id="main-content" className="flex flex-col  justify-center items-center m-4">
+                    <RaffleUI giveaway={giveaway!} />
+                </div>
+            );
+        } else {
+            return (
+                <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
+                    You dont have permissions to view this
+                </div>
+            );
+        }
     } else {
+        // If we dont get a result from the query, the giveaway doesn't exist -> we shouldnt even load the controls
         return (
             <div id="main-content" className="flex flex-col  justify-center items-center m-4 text-4xl">
                 Raffle doesn't exist
