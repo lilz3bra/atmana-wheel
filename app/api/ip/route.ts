d25a15356366dc4500684c450b537e0f48a32356
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-18 20:47:03 -0300

    Removed since it would be agains twitch's ToS

--- app/api/ip/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-
-export async function GET(req: NextRequest) {
-    const headersList = headers();
-    const ip = headersList.get("x-forwarded-for");
-    console.log(ip);
-    return NextResponse.json({ ip: ip });
-}
