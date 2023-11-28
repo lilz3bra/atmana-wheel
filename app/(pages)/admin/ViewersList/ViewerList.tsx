740dec0b947cb74bf1b54f34bec9cc6d4e4dd82d
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-28 18:43:20 -0300

    Fixed the style to be as intended

--- app/(pages)/admin/ViewersList/ViewerList.tsx
+++ app/(pages)/admin/ViewersList/ViewerList.tsx
@@ -16,16 +16,22 @@ const ViewersList = ({ viewers }: Props) => {
     return (
         <>
             <div>Page {page}</div>
-            <ul className=" grid grid-cols-2 justify-center items-center ">
+            <div className="w-fit flex flex-col gap-1 justify- m-auto ">
                 {viewers.map((viewer, index) => (
-                    <div className={`grid grid-cols-2${index % 2 === 0 ? "bg-gray-500" : "bg-gray-600"}`} key={index}>
-                        <div className="w-min flex flex-row gap-4 justify-between p-1 items-center  ">{viewer.viewer.name}</div>
-                        <button onClick={() => console.log(viewer.viewer.name)} className="bg-slate-700 rounded-xl p-1 hover:bg-slate-800">
+                    <div
+                        className={`${index % 2 === 0 ? "bg-gray-500" : "bg-gray-600"} grid grid-cols-2 p-2 align-middle justify-between items-center gap-2`}
+                        style={{
+                            gridTemplateColumns: "auto auto", // Set columns to be the width of their content
+                            gridAutoFlow: "column", // Flow items into columns
+                        }}
+                        key={index}>
+                        <div>{viewer.viewer.name}</div>
+                        <button onClick={() => console.log(viewer.viewer.name)} className="bg-slate-700 rounded-xl p-1 hover:bg-slate-800 w-fit">
                             {viewer.viewer.isBanned ? "Unban" : "Ban"}
                         </button>
                     </div>
                 ))}
-            </ul>
+            </div>
         </>
     );
 };
