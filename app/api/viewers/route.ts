ba92d5446d4485d62fc19f9f3f9989fefcbc7998
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-29 18:25:31 -0300

    + Ban endpoint. Fixed missing data on GET endpoint

--- app/api/viewers/route.ts
+++ app/api/viewers/route.ts
@@ -16,9 +16,26 @@ export async function GET(req: NextRequest) {
 
     const res = await prisma.viewerOnStream.findMany({
         where: { streamerId: thisUser.id },
-        select: { viewer: { select: { name: true, id: true } } },
+        select: { viewer: { select: { name: true, id: true, isBanned: true } } },
         skip: (page - 1) * 20,
         take: 20,
     });
     return NextResponse.json(res);
+}
+
+export async function POST(req: NextRequest) {
+    // Validate authorization
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    const data = await req.json();
+
+    let status = 404;
+
+    if (data.action === "ban" && data.viewerId && typeof data.state === "boolean") {
+        const res = await prisma.viewer.update({ where: { id: data.viewerId }, data: { isBanned: data.state } });
+        if (res) status = 200;
+    }
+
+    return NextResponse.json({}, { status });
 }
