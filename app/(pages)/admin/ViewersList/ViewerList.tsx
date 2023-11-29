ff89bf3a03f4335f44dcc8d9a684d185c6572cc5
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2023-11-29 17:26:05 -0300

    Added pagination

--- app/(pages)/admin/ViewersList/ViewerList.tsx
+++ app/(pages)/admin/ViewersList/ViewerList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
     viewers: {
@@ -10,13 +10,36 @@ interface Props {
         };
     }[];
 }
-const ViewersList = ({ viewers }: Props) => {
+const ViewersList = ({ viewers: initialViewers }: Props) => {
+    const [viewers, setViewers] = useState(initialViewers);
     const [page, setPage] = useState(1);
+
+    const changePage = (change: number) => {
+        if ((viewers.length < 20 && change === 1) || (page === 1 && change === -1)) return;
+        setPage(page + change);
+    };
+
+    useEffect(() => {
+        const fetchViewers = async () => {
+            const res = await fetch(`/api/viewers?page=${page}`);
+            const data = await res.json();
+            setViewers(data);
+        };
+        fetchViewers();
+    }, [page]);
 
     return (
         <>
-            <div>Page {page}</div>
-            <div className="w-fit flex flex-col gap-1 justify- m-auto ">
+            <div className="flex flex-row align-middle justify-center items-center gap-2">
+                <button onClick={() => changePage(-1)} className="bg-slate-700 rounded-xl p-1 hover:bg-slate-800 w-fit">
+                    Prev
+                </button>
+                Page {page}
+                <button onClick={() => changePage(1)} className="bg-slate-700 rounded-xl p-1 hover:bg-slate-800 w-fit">
+                    Next
+                </button>
+            </div>
+            <div className="w-fit flex flex-col gap-1 m-auto ">
                 {viewers.map((viewer, index) => (
                     <div
                         className={`${index % 2 === 0 ? "bg-gray-500" : "bg-gray-600"} grid grid-cols-2 p-2 align-middle justify-between items-center gap-2`}
