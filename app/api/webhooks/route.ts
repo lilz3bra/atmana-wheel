4db18b999c91dbc69e407f491dc1f76b42260f8d
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-01-19 18:52:18 -0300

    Verify ga exists first. Update usernames if they exist (to avoid mismatches)

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -11,24 +11,29 @@ export async function POST(req: Request) {
         return new Response(data.challenge, { status: 200, headers: { "Content-Type": "text/plain" } });
     } else {
         if (await verifyMessage(req, msg)) {
-            const viewer = await prisma.viewer.upsert({
-                where: { twitchId: data.event.user_id },
-                update: {},
-                create: { name: data.event.user_name, twitchId: data.event.user_id, isBanned: false, isApproved: false },
-            });
             const giveaway = await prisma.giveaways.findFirst({ where: { twitchId: data.event.reward.id }, select: { id: true, creatorId: true } });
-            const viewerOnStream = await prisma.viewerOnStream.upsert({
-                where: {
-                    UniqueViewerOnStream: {
-                        streamerId: giveaway?.creatorId!,
-                        viewerId: viewer.id!,
+            if (giveaway) {
+                const viewer = await prisma.viewer.upsert({
+                    where: { twitchId: data.event.user_id },
+                    update: { name: data.event.user_name },
+                    create: { name: data.event.user_name, twitchId: data.event.user_id, isBanned: false, isApproved: false },
+                });
+                const viewerOnStream = await prisma.viewerOnStream.upsert({
+                    where: {
+                        UniqueViewerOnStream: {
+                            streamerId: giveaway.creatorId,
+                            viewerId: viewer.id,
+                        },
                     },
-                },
-                update: {},
-                create: { streamerId: giveaway?.creatorId!, viewerId: viewer.id },
-            });
-            const result = await prisma.giveawayRedemptions.create({ data: { viewerId: viewer.id!, redeemedAt: data.event.redeemed_at, giveawayId: giveaway?.id! } });
-            return NextResponse.json({}, { status: 200 });
+                    update: {},
+                    create: { streamerId: giveaway.creatorId, viewerId: viewer.id },
+                });
+                const result = await prisma.giveawayRedemptions.create({ data: { viewerId: viewer.id, redeemedAt: data.event.redeemed_at, giveawayId: giveaway.id } });
+                return NextResponse.json({}, { status: 200 });
+            } else {
+                console.log("Invalid giveaway requested");
+                return NextResponse.json({}, { status: 403 });
+            }
         }
         console.log("Invalid message received");
         return NextResponse.json({}, { status: 403 });
