ec2b793043df9fad65f4627b6fd0a8f0905fe8e7
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-01-28 20:42:17 -0300

    Performance Test #2

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -8,22 +8,27 @@ export async function POST(req: Request) {
     const startTime = performance.now();
     const msg = await req.text();
     const data = await JSON.parse(msg);
+    console.log("Message:", msg);
     console.log("Parsing runtime:", performance.now() - startTime);
+    let partialTime = performance.now();
     if (data.subscription.status === "webhook_callback_verification_pending") {
         return new Response(data.challenge, { status: 200, headers: { "Content-Type": "text/plain" } });
     } else {
         if (await verifyMessage(req, msg)) {
-            console.log("Verification runtime:", performance.now() - startTime);
+            console.log("Verification runtime:", performance.now() - partialTime);
+            partialTime = performance.now();
             const giveaway = await prisma.giveaways.findFirst({ where: { twitchId: data.event.reward.id }, select: { id: true, creatorId: true } });
-            console.log("Giveaway fetch runtime:", performance.now() - startTime);
+            console.log("Giveaway fetch runtime:", performance.now() - partialTime);
+            partialTime = performance.now();
             if (giveaway) {
                 const viewer = await prisma.viewer.upsert({
                     where: { twitchId: data.event.user_id },
                     update: { name: data.event.user_name },
                     create: { name: data.event.user_name, twitchId: data.event.user_id, isBanned: false, isApproved: false },
                 });
-                console.log("Viewer upsert runtime:", performance.now() - startTime);
-                const viewerOnStream = await prisma.viewerOnStream.upsert({
+                console.log("Viewer upsert runtime:", performance.now() - partialTime);
+                partialTime = performance.now();
+                prisma.viewerOnStream.upsert({
                     where: {
                         UniqueViewerOnStream: {
                             streamerId: giveaway.creatorId,
@@ -33,10 +38,11 @@ export async function POST(req: Request) {
                     update: {},
                     create: { streamerId: giveaway.creatorId, viewerId: viewer.id },
                 });
-                console.log("ViewerOnStream upsert runtime:", performance.now() - startTime);
-                const result = await prisma.giveawayRedemptions.create({ data: { viewerId: viewer.id, redeemedAt: data.event.redeemed_at, giveawayId: giveaway.id } });
-                console.log("Redemption create runtime:", performance.now() - startTime);
-
+                console.log("ViewerOnStream upsert runtime:", performance.now() - partialTime);
+                partialTime = performance.now();
+                prisma.giveawayRedemptions.create({ data: { viewerId: viewer.id, redeemedAt: data.event.redeemed_at, giveawayId: giveaway.id } });
+                console.log("Redemption create runtime:", performance.now() - partialTime);
+                console.log("Total runtime:", performance.now() - startTime);
                 return NextResponse.json({}, { status: 200 });
             } else {
                 console.log("Invalid giveaway requested");
