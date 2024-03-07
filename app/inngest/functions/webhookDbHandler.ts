3c887daa21f446ad8d539bd1a507f69f02a56390
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-03-07 18:53:45 -0300

    Reduce the steps used, use batching

--- app/inngest/functions/webhookDbHandler.ts
+++ app/inngest/functions/webhookDbHandler.ts
@@ -1,41 +1,49 @@
 import { prisma } from "@/lib/prisma";
 import { inngest } from "../inngest.client";
 
-export const streamViewers = inngest.createFunction({ id: "streamViewers", name: "Viewers on stream" }, { event: "webhook.claim" }, async ({ event, step }) => {
-    const creatorId: string = event.data.creatorId;
-    const giveawayId: string = event.data.giveawayId;
-    const viewerId: string = event.data.viewerId;
-    const viewerName: string = event.data.viewerName;
+interface CountMap {
+    [key: string]: { viewerId: string; giveawayId: string; creatorId: string; viewerName: string; count: number };
+}
 
-    const viewer = await step.run("fetch-viewer", async () => {
-        const v = await prisma.viewer.findFirst({ where: { twitchId: viewerId } });
-        if (v) {
-            if (v.name !== viewerName) {
-                await prisma.viewer.update({ where: { id: v.id }, data: { name: viewerName } });
-            }
-            return v;
-        } else {
-            const vi = await prisma.viewer.create({ data: { name: viewerName, twitchId: viewerId, isBanned: false, isApproved: false } });
-            return vi;
-        }
-    });
+export const streamViewers = inngest.createFunction(
+    { id: "streamViewers", name: "Viewers on stream", batchEvents: { maxSize: 100, timeout: "5s" } },
+    { event: "webhook.claim" },
+    async ({ events, step }) => {
+        const data = events.reduce((acc, item) => {
+            const { viewerName, viewerId, giveawayId, creatorId } = item.data;
+            const key = `${viewerId}_${giveawayId}`;
+            acc[key] = acc[key] || { viewerId, giveawayId, creatorId, viewerName, count: 0 };
+            acc[key].count++;
+            return acc;
+        }, {} as CountMap);
 
-    if (viewer) {
-        const viewerOnStream = step.run("upsert-viewer-on-stream", async () => {
-            return await prisma.streamViewers.upsert({
-                where: { UniqueViewerForCreator: { creatorId: creatorId, viewerId: viewer.id } },
-                update: {},
-                create: { creatorId: creatorId, viewerId: viewer.id },
+        const result = await step.run("record data to DB", async () => {
+            return Object.entries(data).forEach(async ([key, entry]) => {
+                try {
+                    let viewer = await prisma.viewer.findFirst({ where: { twitchId: entry.viewerId }, select: { id: true, name: true } });
+                    if (viewer) {
+                        if (viewer.name !== entry.viewerName) {
+                            await prisma.viewer.update({ where: { id: viewer.id }, data: { name: entry.viewerName } });
+                        }
+                    } else {
+                        viewer = await prisma.viewer.create({ data: { name: entry.viewerName, twitchId: entry.viewerId, isBanned: false, isApproved: false } });
+                    }
+                    await prisma.streamViewers.upsert({
+                        where: { UniqueViewerForCreator: { creatorId: entry.creatorId, viewerId: viewer.id } },
+                        update: {},
+                        create: { creatorId: entry.creatorId, viewerId: viewer.id },
+                    });
+
+                    return await prisma.giveawayRedemptions.upsert({
+                        where: { ViewerRedemptions: { viewerId: viewer.id, giveawayId: entry.giveawayId } },
+                        update: { ammount: { increment: entry.count } },
+                        create: { viewerId: viewer.id, giveawayId: entry.giveawayId },
+                    });
+                } catch (err) {
+                    console.error("Error inserting:", entry);
+                }
             });
         });
-        const redemption = step.run("insert-redemption", async () => {
-            return await prisma.giveawayRedemptions.upsert({
-                where: { ViewerRedemptions: { viewerId: viewer.id, giveawayId: giveawayId } },
-                update: { ammount: { increment: 1 } },
-                create: { viewerId: viewer.id, giveawayId: giveawayId },
-            });
-        });
-        const [createdVoS, createdRedemption] = await Promise.all([viewerOnStream, redemption]);
-        return { createdVoS, createdRedemption };
+        return { success: true, recorded: result };
     }
-});
+);
