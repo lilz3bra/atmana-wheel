1c69b16493039aae3f31c3e12e8c41be2f9dbc03
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-03-08 21:26:15 -0300

    get rid of authorization to properly test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,12 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { Inngest } from "inngest";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
 
 export async function POST(req: NextRequest) {
-    const session = await getServerSession(authOptions);
-    if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     const n = Number(req.nextUrl.searchParams.get("n")) || 1;
     const id = req.nextUrl.searchParams.get("id");
     if (id === null) return NextResponse.json({});
