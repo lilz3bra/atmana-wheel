cfab4ef6114372ab5962ca151a969f480030c016
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-09-22 10:40:31 -0300

    added temporary dev pages

---
+++ app/(pages)/tst/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Wrapper from "./wrapper";
+
+export default function WheelPage() {
+    return (
+        <div className="bg-white">
+            <Wrapper />
+        </div>
+    );
+}
