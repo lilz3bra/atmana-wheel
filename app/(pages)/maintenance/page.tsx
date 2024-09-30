cbd4032cac4b62f973f99ed8f427e7dc97341ef7
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-09-30 19:53:39 -0300

    Maintenance

---
+++ app/(pages)/maintenance/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="flex justify-center h-full w-full text-2xl absolute top-1/2">
+            Under maintenance. Come back later
+        </div>
+    );
+};
+
+export default page;
