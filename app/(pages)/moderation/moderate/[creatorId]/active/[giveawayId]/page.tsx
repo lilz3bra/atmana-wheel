78e9fffe656bbc43b5076a5c395352ce6e8e68b2
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-10-01 12:05:19 -0300

    Revert "Merge pull request #83 from lilz3bra/moderation"
    This reverts commit 1f057941234906a1690395bf369ee0f87bdcf534, reversing
    changes made to 43cb292a3de9df625044ba0de7e2ff9da472f922.

--- app/(pages)/moderation/moderate/[creatorId]/active/[giveawayId]/page.tsx
+++ app/(pages)/moderation/moderate/[creatorId]/active/[giveawayId]/page.tsx
@@ -10,28 +10,23 @@ const page = async ({ params }: Props) => {
     const viewers = await prisma.giveawayRedemptions.findMany({
         where: { giveawayId: params.giveawayId },
         select: { viewer: { select: { id: true, name: true } }, ammount: true },
-        orderBy: { ammount: "desc" },
     });
     const totalTickets = viewers.reduce((sum, object) => sum + object.ammount, 0);
-
-    const userElements: JSX.Element[] = [];
-    Object.keys(viewers!).forEach((_, index) => {
-        const weight = viewers![index].ammount;
-        const percentage = ((weight / totalTickets) * 100).toFixed(2);
-        userElements.push(
-            <p key={viewers![index].viewer.name}>
-                <span className="font-bold">{viewers![index].viewer.name}</span>: {weight} entr{weight > 1 ? "ies" : "y"} <span className="italic">({percentage}%)</span>
-            </p>
-        );
-    });
-
+    console.log(viewers);
     return (
         <div id="main-content" className="flex flex-col  justify-center items-center m-4">
             <>
                 <h1 className="font-bold text-xl text-center m-4">
                     {viewers.length} Participants ({totalTickets} entries)
                 </h1>
-                <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">{userElements}</div>
+                <div className="m-auto w-2/3 h-1/2 justify-center text-center gap-2">
+                    {viewers.map((entry) => (
+                        <p key={entry.viewer.name}>
+                            <span className="font-bold">{entry.viewer.name}</span>: {entry.ammount} entr{entry.ammount > 1 ? "ies" : "y"}{" "}
+                            <span className="italic">({((entry.ammount / totalTickets) * 100).toFixed(2)}%)</span>
+                        </p>
+                    ))}
+                </div>
             </>
         </div>
     );
