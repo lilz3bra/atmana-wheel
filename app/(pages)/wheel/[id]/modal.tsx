0212ca9d5e6e4387788b57b51c4502f258da3e1a
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2024-10-07 20:06:54 -0300

    fix: stop modal from complaining

--- app/(pages)/wheel/[id]/modal.tsx
+++ app/(pages)/wheel/[id]/modal.tsx
@@ -23,25 +23,31 @@ function Modal({
         setIsOpen(false);
         onClose();
     };
-
     return (
-        <ReactModal
-            isOpen={isOpen}
-            id="modal"
-            contentLabel="Register dialog"
-            className="absolute block top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-full w-2/3 h-5/6 rounded-xl bg-slate-800"
-            overlayClassName="fixed top-0 right-0 bottom-0 left-0 bg-black/50"
-            appElement={root}
-            onRequestClose={handleCloseModal}>
-            <button
-                className="flex absolute justify-center items-center top-1 right-1 rounded-full w-6 h-6 bg-orange-900 text-center text-gray-950 font-bold hover:bg-red-400 hover:text-gray-700"
-                onClick={handleCloseModal}>
-                X
-            </button>
-            {entries ? (
-                <Wheel entradas={entries} callback={returnCallback} cerrando={isClosing} totalEntradas={totalCount} />
-            ) : null}
-        </ReactModal>
+        <div className="container">
+            <ReactModal
+                isOpen={isOpen}
+                id="modal"
+                contentLabel="Register dialog"
+                className="absolute block top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-full w-2/3 h-5/6 rounded-xl bg-slate-800"
+                overlayClassName="fixed top-0 right-0 bottom-0 left-0 bg-black/50"
+                ariaHideApp={false}
+                onRequestClose={handleCloseModal}>
+                <button
+                    className="flex absolute justify-center items-center top-1 right-1 rounded-full w-6 h-6 bg-orange-900 text-center text-gray-950 font-bold hover:bg-red-400 hover:text-gray-700"
+                    onClick={handleCloseModal}>
+                    X
+                </button>
+                {entries ? (
+                    <Wheel
+                        entradas={entries}
+                        callback={returnCallback}
+                        cerrando={isClosing}
+                        totalEntradas={totalCount}
+                    />
+                ) : null}
+            </ReactModal>
+        </div>
     );
 }
 export default Modal;
